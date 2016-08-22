5bc7722d6d246c4e1a4a12c08d1157ed94df7ac3
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-08-22 14:52:08 +0100

    added topup method and tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(money)
+    @balance += money
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,14 @@
-require 'oystercard.rb'
+require 'oystercard'
 
 describe Oystercard do
 
   it 'should initialize with a balance' do
     expect(subject.balance).to eq 0
   end
+
+  it "should increment the count by 2" do
+    money = 76
+    expect{subject.top_up(money)}.to change{subject.balance}.by(money)
+   end
 
 end
