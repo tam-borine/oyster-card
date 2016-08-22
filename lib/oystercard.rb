de2b3f8cc5cba48961b6948140eac6ad780edac9
jameshamann <jameshamann@me.com>
2016-08-22 16:32:16 +0100

    default limit and balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,31 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :limit
 
-  def initialize
-    @balance = 0
+  DEFAULT_LIMIT = 90
+  DEFAULT_BALANCE = 0
+
+  def initialize(limit = DEFAULT_LIMIT, balance = DEFAULT_BALANCE)
+    @limit = limit
+    @balance = balance
+    fail 'Balance cannot be larger than limit' if balance > limit
   end
 
   def top_up(amount)
+    fail 'Balance limit reached' if full? || amount > 90
     @balance += amount
+    top_up_confirmation
   end
+
+  private
+
+  def full?
+    balance >= 90
+  end
+
+  def top_up_confirmation
+    "Your new balance is #{balance}"
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,19 +2,49 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard) {described_class.new}
-  it 'instantiates with a balance of 0' do
-    expect(oystercard.balance).to eq 0
+
+  describe '#initialize' do
+    it 'instantiates with a balance of 0' do
+      expect(oystercard.balance).to eq described_class::DEFAULT_BALANCE
+    end
+
+    it 'has a default limit' do
+      expect(oystercard.limit).to eq described_class::DEFAULT_LIMIT
+    end
+
+    it 'sets a given limit' do
+      oystercard = Oystercard.new 100
+      expect(oystercard.limit).to eq 100
+    end
+
+    it 'sets a given balance' do
+      oystercard = Oystercard.new 100, 50
+      expect(oystercard.balance).to eq 50
+    end
+
+    it 'raises an error when instantiated balance is larger than limit' do
+      msg = 'Balance cannot be larger than limit'
+      expect {Oystercard.new 50, 100}.to raise_error msg
+    end
+
   end
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
+  describe '#top_up' do
 
-  it 'tops up the card' do
-    expect(oystercard.top_up(10)).to eq 10
+    it 'confirms new balance after top-up' do
+      msg = "Your new balance is 10"
+      expect(oystercard.top_up(10)).to eq msg
+    end
+
+    it 'updates the balance' do
+      oystercard.top_up(10)
+      expect(oystercard.balance).to eq 10
+    end
+
+    it 'raises error if limit reached' do
+      oystercard.top_up(90)
+      msg = 'Balance limit reached'
+      expect {oystercard.top_up(1)}.to raise_error msg
+    end
   end
-
-  it 'updates the balance' do
-    oystercard.top_up(10)
-    expect(oystercard.balance).to eq 10
-  end
-
 end
