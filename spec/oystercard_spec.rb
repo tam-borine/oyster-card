172e07af261d1678337c6a4d6219c667818e219c
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-22 13:37:12 +0100

    balance method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+  it 'instantiates with a balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
 
 end
