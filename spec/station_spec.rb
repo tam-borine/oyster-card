8268fd04f74a184b3dbfcd57409b0219c1a4a136
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-08-23 12:48:28 +0100

    Added station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe Station do
+
+  it "inits zone" do
+    subject = Station.new("euston", 1)
+    expect(subject.zone).to eq(1)
+end
+
+it "inits name" do
+  subject = Station.new("euston", 1)
+  expect(subject.name).to eq("euston")
+end
+end
